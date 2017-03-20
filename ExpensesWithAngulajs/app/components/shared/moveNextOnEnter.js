20e5b47d8245d4179d8caab3feaf26caffa3e655
Margarito <jimenez8382@gmail.com>
2017-03-20 17:02:31 -0500

    adding directive to move on next control when the user press enter in the Expenses form

---
+++ ExpensesWithAngulajs/app/components/shared/moveNextOnEnter.js
@@ -0,0 +1,19 @@
+﻿appDirectives.directive("moveNextOnEnter", function () {
+    return {
+        restrict: 'A',
+        link: function ($scope, elem, attrs) {
+
+            elem.bind('keydown', function (e) {
+                var code = e.keyCode || e.which;
+                if (code === 13) {
+                    if (attrs.tabindex != undefined) {
+                        var currentTabIndex = attrs.tabindex;
+                        var nextTabIndex = parseInt(attrs.tabindex) + 1;
+                        $("[tabindex=" + nextTabIndex + "]").focus();
+                    }
+
+                }
+            });
+        }
+    }
+});
