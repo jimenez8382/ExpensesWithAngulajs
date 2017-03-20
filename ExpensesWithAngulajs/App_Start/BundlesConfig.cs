695f21d8717cd219bd7378c3ba569e90011758c4
Margarito <jimenez8382@gmail.com>
2017-03-20 13:44:28 -0500

    first commit

---
+++ ExpensesWithAngulajs/App_Start/BundlesConfig.cs
@@ -0,0 +1,30 @@
+﻿using System.Web.Optimization;
+namespace ExpensesWithAngulajs.App_Start
+{
+   public class BundlesConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            //Scripts
+            RegisterScripts(bundles);
+            //Styles
+            RegisterStyles(bundles);
+        }
+
+        private static void RegisterStyles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/Styles").Include("~/Content/bootstrap.min.css")
+                    .Include("~/Content/materialize/css/materialize.min.css")
+                    .Include("~/Content/angular-block-ui.min.css")
+                    .Include("~/Content/Site.css")
+                );
+        }
+
+        private static void RegisterScripts(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include("~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/bundles/js").Include("~/Content/js/bootstrap.min.js")
+                .Include("~/Scripts/materializematerialize.min.js"));
+        }
+    }
+}
---
+++ ExpensesWithAngulajs/Global.asax.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web.Http;
+using ExpensesWithAngulajs.App_Start;
+using System.Web.Optimization;
+namespace ExpensesWithAngulajs
+{
+    public class Global : HttpApplication
+    {
+        void Application_Start(object sender, EventArgs e)
+        {
+            // Code that runs on application startup
+            AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundlesConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
